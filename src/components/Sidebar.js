5c204047fc06213b6874745355b2c75156d04319
lucas-palmeida <lucaspacheco95@gmail.com>
2022-11-21 11:02:59 -0300

    refact: adc de verificacao dos valores passados nas func de troca de nome e imagem do usuario

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,15 +4,30 @@ import Sugestoes from "./Sugestoes";
 export default function Sidebar() {
   const [nome, setNome] = React.useState("Catana");
   const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg")
+
+  function verificaNome() {
+    const nomeRecebido = prompt("Digite o link da imagem:");
+    if(nomeRecebido) {
+      setNome(nomeRecebido);
+    }
+  }
+
+  function verificaImagem() {
+    const imagemRecebida = prompt("Digite o link da imagem:");
+    if(imagemRecebida) {
+      setImagem(imagemRecebida);
+    }
+  }
+  
   return (
     <div className="sidebar">
       <div className="usuario" data-test="user">
-        <img src={imagem} alt={nome} onClick={() => setImagem(prompt("Digite o link da imagem:"))} data-test="profile-image"/>
+        <img src={imagem} alt={nome} onClick={verificaImagem} data-test="profile-image"/>
         <div className="texto">
           <strong>catanacomics</strong>
           <span data-test="name">
             {nome}
-            <ion-icon name="pencil" onClick={() => setNome(prompt("Digite o novo nome:"))} data-test="edit-name"></ion-icon>
+            <ion-icon name="pencil" onClick={verificaNome} data-test="edit-name"></ion-icon>
           </span>
         </div>
       </div>
